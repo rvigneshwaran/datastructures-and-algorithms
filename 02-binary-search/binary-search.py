84101e1b5da5b5b56625f276b4590cf99da77504
Vigneshwaran Rethenasekaran <redhatvicky@gmail.com>
2022-07-17 05:57:37 +0530

    updated folder sequencing and removed unnecessary files

---
+++ 02-binary-search/binary-search.py
@@ -0,0 +1,14 @@
+
+input_list_sample  = [11,34,21,1,45,67,1,90,12,8,89,23,65,43,32]
+
+class BinarySearch:
+    
+    def __init__(self):
+        print("Initializing Components")
+        
+    def find_middle_element(self,input_list):
+        list_size = len(input_list)
+        if list_size % 2 == 0:
+            
+    def binary_search(self,search_list,search_element):
+        return ""
