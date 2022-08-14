d886c360ef57d62989ae8d50a3787db76862e7a1
Vigneshwaran Rethenasekaran <redhatvicky@gmail.com>
2022-08-14 14:49:07 +0530

    Fixed Flow t determine if the element is not present in the list

--- 02-binary-search/binary-search.py
+++ 02-binary-search/binary-search.py
@@ -1,5 +1,5 @@
 
-search_element=43
+search_element=8
 input_list_sample  = [1, 8, 11, 12, 12, 21, 23, 32, 34, 43, 45, 65, 67, 89, 90]
 class BinarySearch:
     
@@ -8,25 +8,31 @@ class BinarySearch:
         
 # This below function is intended to do a binary search using a recursive approach
     def binary_search(self,search_list,low_index,high_index):
+        # Setting default search index to -1 to denote the element is not present in the list
+        search_index=-1
         print("low_index :: "+str(low_index)+ " high_index :: "+str(high_index))
-        if high_index >= 1:
+        if high_index >= low_index:
             mid_index = (low_index + high_index) // 2
             print("Mid Index :: "+str(mid_index)+" Mid Element :: "+str(str(search_list[mid_index])))
             if search_list[mid_index] == search_element:
-                print("The searched element :: "+str(search_element)+" is found at the index :: "+str(mid_index))
+                search_index = mid_index
             elif search_element < search_list[mid_index]:
                 print("Element should be searched on the left Half of the list")
                 high_index = mid_index - 1
                 print("low_index :: "+str(low_index)+ " high_index :: "+str(high_index))
-                self.binary_search(search_list,low_index,high_index)
+                return self.binary_search(search_list,low_index,high_index)
             elif search_element > search_list[mid_index]:
                 print("Element should be searched on the right side of the list")
                 low_index = mid_index + 1
                 print("low_index :: "+str(low_index)+ " high_index :: "+str(high_index))
-                self.binary_search(search_list,low_index,high_index)
-        else:
-            print("The searched element :: "+str(search_element)+" is not found in the list ")
+                return self.binary_search(search_list,low_index,high_index)
+        return search_index
+            
     
 binary_search_ins=BinarySearch()
 final_index = len(input_list_sample) - 1
 found_index = binary_search_ins.binary_search(input_list_sample,0,final_index)
+if found_index == -1:
+    print("The searched element :: "+str(search_element)+" is not found in the list ")
+else:
+    print("The searched element :: "+str(search_element)+" is found at the index :: "+str(found_index))
